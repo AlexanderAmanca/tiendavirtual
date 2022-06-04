97f128e42ee73d60f2533697bd038f5aa4fdb967
YasminNF <kapricornio_126@hotmail.com>
2022-06-04 18:03:09 -0500

    login vista mod

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -4,11 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Abel OSH">
+    <meta name="theme-color" content="#009688">
+    <link rel="shortcut icon" href="<?= media();?>/images/favicon.ico">
     <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <!-- Font-icon css-->
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Login - Vali Admin</title>
+    <link rel="stylesheet" type="text/css" href="<?= media();?>/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?= media();?>/css/style.css">
+    
+    <title><?= $data['page_tag']; ?></title>
   </head>
   <body>
     <section class="material-half-bg">
@@ -16,61 +19,61 @@
     </section>
     <section class="login-content">
       <div class="logo">
-        <h1>Vali</h1>
+        <h1><?= $data['page_title']; ?></h1>
       </div>
       <div class="login-box">
-        <form class="login-form" action="index.html">
-          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>SIGN IN</h3>
+        <div id="divLoading" >
+          <div>
+            <img src="<?= media(); ?>/images/loading.svg" alt="Loading">
+          </div>
+        </div>
+        <form class="login-form" name="formLogin" id="formLogin" action="">
+          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>INICIAR SESIÓN</h3>
           <div class="form-group">
-            <label class="control-label">USERNAME</label>
-            <input class="form-control" type="text" placeholder="Email" autofocus>
+            <label class="control-label">USUARIO</label>
+            <input id="txtEmail" name="txtEmail" class="form-control" type="email" placeholder="Email" autofocus>
           </div>
           <div class="form-group">
-            <label class="control-label">PASSWORD</label>
-            <input class="form-control" type="password" placeholder="Password">
+            <label class="control-label">CONTRASEÑA</label>
+            <input id="txtPassword" name="txtPassword" class="form-control" type="password" placeholder="Contraseña">
           </div>
           <div class="form-group">
             <div class="utility">
-              <div class="animated-checkbox">
-                <label>
-                  <input type="checkbox"><span class="label-text">Stay Signed in</span>
-                </label>
-              </div>
-              <p class="semibold-text mb-2"><a href="#" data-toggle="flip">Forgot Password ?</a></p>
+              <p class="semibold-text mb-2"><a href="#" data-toggle="flip">¿Olvidaste tu contraseña?</a></p>
             </div>
           </div>
+          <div id="alertLogin" class="text-center"></div>
           <div class="form-group btn-container">
-            <button class="btn btn-primary btn-block"><i class="fa fa-sign-in fa-lg fa-fw"></i>SIGN IN</button>
+            <button type="submit" class="btn btn-primary btn-block"><i class="fas fa-sign-in-alt"></i> INICIAR SESIÓN</button>
           </div>
         </form>
-        <form class="forget-form" action="index.html">
-          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-lock"></i>Forgot Password ?</h3>
+        <form id="formRecetPass" name="formRecetPass" class="forget-form" action="">
+          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-lock"></i>¿Olvidaste contraseña?</h3>
           <div class="form-group">
             <label class="control-label">EMAIL</label>
-            <input class="form-control" type="text" placeholder="Email">
+            <input id="txtEmailReset" name="txtEmailReset" class="form-control" type="email" placeholder="Email">
           </div>
           <div class="form-group btn-container">
-            <button class="btn btn-primary btn-block"><i class="fa fa-unlock fa-lg fa-fw"></i>RESET</button>
+            <button type="submit" class="btn btn-primary btn-block"><i class="fa fa-unlock fa-lg fa-fw"></i>REINICIAR</button>
           </div>
           <div class="form-group mt-3">
-            <p class="semibold-text mb-0"><a href="#" data-toggle="flip"><i class="fa fa-angle-left fa-fw"></i> Back to Login</a></p>
+            <p class="semibold-text mb-0"><a href="#" data-toggle="flip"><i class="fa fa-angle-left fa-fw"></i> Iniciar sesión</a></p>
           </div>
         </form>
       </div>
     </section>
-    <!-- Essential javascripts for application to work-->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-    <!-- The javascript plugin to display page loading on top-->
-    <script src="js/plugins/pace.min.js"></script>
-    <script type="text/javascript">
-      // Login Page Flipbox control
-      $('.login-content [data-toggle="flip"]').click(function() {
-      	$('.login-box').toggleClass('flipped');
-      	return false;
-      });
+    <script>
+        const base_url = "<?= base_url(); ?>";
     </script>
+    <!-- Essential javascripts for application to work-->
+    <script src="<?= media(); ?>/js/jquery-3.3.1.min.js"></script>
+    <script src="<?= media(); ?>/js/popper.min.js"></script>
+    <script src="<?= media(); ?>/js/bootstrap.min.js"></script>
+    <script src="<?= media(); ?>/js/fontawesome.js"></script>
+    <script src="<?= media(); ?>/js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="<?= media(); ?>/js/plugins/pace.min.js"></script>
+    <script type="text/javascript" src="<?= media();?>/js/plugins/sweetalert.min.js"></script>
+    <script src="<?= media(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
   </body>
 </html>
