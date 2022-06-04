2ee7930ee1a19fe8f7f89f823a1fefb655347351
AlexanderAmanca <alexanderamancaz@gmail.com>
2022-06-04 03:58:22 -0500

    footeradminCorreciones

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -1,5 +1,6 @@
-    <script>
+<script>
         const base_url = "<?= base_url(); ?>";
+        const smony = "<?= SMONEY; ?>";
     </script>
     <!-- Essential javascripts for application to work-->
     <script src="<?= media(); ?>/js/jquery-3.3.1.min.js"></script>
@@ -7,11 +8,11 @@
     <script src="<?= media(); ?>/js/bootstrap.min.js"></script>
     <script src="<?= media(); ?>/js/main.js"></script>
     <script src="<?= media();?>/js/fontawesome.js"></script>
-    <script src="<?= media(); ?>/js/functions_admin.js"></script>
     <!-- The javascript plugin to display page loading on top-->
     <script src="<?= media(); ?>/js/plugins/pace.min.js"></script>
     <!-- Page specific javascripts-->
-     <script type="text/javascript" src="<?= media(); ?>/js/plugins/sweetalert.min.js"></script>
+    <script type="text/javascript" src="<?= media(); ?>/js/plugins/sweetalert.min.js"></script>
+    <script type="text/javascript" src="<?= media(); ?>/js/tinymce/tinymce.min.js"></script>
 
     <!-- Data table plugin-->
     <script type="text/javascript" src="<?= media(); ?>/js/plugins/jquery.dataTables.min.js"></script>
@@ -22,14 +23,11 @@
     <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
     <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
-
-    <script type="text/javascript" src="<?= media();?>/js/functions-admin.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="<?= media();?>/js/datepicker/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="<?= media();?>/js/functions_admin.js"></script>
     <script src="<?= media(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
-
-    <?php if($data['page_name']=="rol_usuario"){ ?>
-    <script src="<?=media(); ?>/js/functions_roles.js"></script>
-    <?php if($data['page_name']=="usuarios"){ ?>
-    <script src="<?=media(); ?>/js/functions_usuarios.js"></script>
-    <?php } ?>
   </body>
 </html>
