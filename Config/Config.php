75e517e2f77f851a55982c471de56eb802006bf7
jfranco666 <xjeanfranco1995@gmail.com>
2022-07-24 21:26:14 -0500

    confg

--- Config/Config.php
+++ Config/Config.php
@@ -1,6 +1,7 @@
 <?php 
-	//define BASE_URL
-	const BASE_URL = "http://localhost/tiendavirtual";
+	
+	//define("BASE_URL", "http://localhost/tienda_virtual/");
+	const BASE_URL = "http://localhost/tienda_virtual";
 
 	//Zona horaria
 	date_default_timezone_set('America/Guatemala');
@@ -11,25 +12,34 @@
 	const DB_USER = "root";
 	const DB_PASSWORD = "";
 	const DB_CHARSET = "utf8";
-	const DB_CHARSET = "charset=utf8";
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
 	const SPD = ".";
 	const SPM = ",";
 
 	//Simbolo de moneda
-	const SMONEY = "S/";
+	const SMONEY = "Q";
 
 	//Datos envio de correo
 	const NOMBRE_REMITENTE = "Tienda Virtual";
-	const EMAIL_REMITENTE = "no-reply@zapateriajeppeto.com";
+	const EMAIL_REMITENTE = "no-reply@abelosh.com";
 	const NOMBRE_EMPESA = "Tienda Virtual";
 	const WEB_EMPRESA = "www.abelosh.com";
+
+	const CAT_SLIDER = "1,2,3";
+	const CAT_BANNER = "4,5,6";
+
+	//Datos para Encriptar / Desencriptar
+	const KEY = 'abelosh';
+	const METHODENCRIPT = "AES-128-ECB";
+
+	//Envío
+	const COSTOENVIO = 50;
 	
 
-	const CAT_SLIDER ="1,2,3";
-	const CAT_BANNER ="4,5,6";
-	const WEB_EMPRESA = "www.zapateriajeppeto.com";
+
+
+	
 
 
  ?>
